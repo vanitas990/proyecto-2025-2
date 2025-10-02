99af6c79058d6c8180dace500cdf47e1c66d3327
Nestor2DA <vanitasnocarte804@gmail.com>
2025-10-02 10:40:42 -0500

    Archivo vista de bienvenida

---
+++ resources/views/bienvenidos.blade.php
@@ -0,0 +1,4 @@
+
+<h1>How are you?!1!</h1>
+<h1>Nestor!2!</h1>
+<h1>Estoy aprendiendo!!3!</h1>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/greeting', function () {
+    return 'Hello friends';
+});
+
+Route::get('/bienvenidos', function () {
+    return view(view:'bienvenidos');
+});
